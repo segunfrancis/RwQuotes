523f719a3b16e1b25c71d79815dcf64211ea0d27
Segun Francis <francis4dblues@gmail.com>
2021-04-21 15:21:14 +0100

    update github actions file

--- app/src/main/java/com/raywenderlich/android/rwquotes/ui/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/rwquotes/ui/MainActivity.kt
@@ -55,94 +55,94 @@ import com.raywenderlich.android.rwquotes.ui.viewmodel.QuotesViewModel
  */
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var quotesViewModel: QuotesViewModel
-  private lateinit var quoteAdapter: QuoteAdapter
+    private lateinit var quotesViewModel: QuotesViewModel
+    private lateinit var quoteAdapter: QuoteAdapter
 
-  private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
 
-  override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-    setTheme(R.style.AppTheme)
-    super.onCreate(savedInstanceState)
-    activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(activityMainBinding.root)
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
-    quotesViewModel = ViewModelProvider(
-        this,
-        QuoteViewModelFactory(QuotesRepositoryImpl(application = application))
-    ).get(QuotesViewModel::class.java)
+        quotesViewModel = ViewModelProvider(
+                this,
+                QuoteViewModelFactory(QuotesRepositoryImpl(application = application))
+        ).get(QuotesViewModel::class.java)
 
 
-    quotesViewModel.dataLoading.observe(this, Observer { value ->
-      value?.let { show ->
-        activityMainBinding.loadingSpinner.visibility = if (show) View.VISIBLE else View.GONE
-      }
-    })
+        quotesViewModel.dataLoading.observe(this, Observer { value ->
+          value?.let { show ->
+            activityMainBinding.loadingSpinner.visibility = if (show) View.VISIBLE else View.GONE
+          }
+        })
 
-    quotesViewModel.getAllQuotes().observe(this, Observer<List<Quote>> {
-      quoteAdapter.setQuotes(it)
-    })
+        quotesViewModel.getAllQuotes().observe(this, Observer<List<Quote>> {
+          quoteAdapter.setQuotes(it)
+        })
 
-    quoteAdapter = QuoteAdapter(clickListener = object : ClickListener {
-      override fun onClick(quote: Quote) {
-        val intent = Intent(this@MainActivity, AddEditActivity::class.java)
-        intent.apply {
-          putExtra(AddEditActivity.EXTRA_ID, quote.id)
-          putExtra(AddEditActivity.EXTRA_AUTHOR, quote.author)
-          putExtra(AddEditActivity.EXTRA_TEXT, quote.text)
-          putExtra(AddEditActivity.EXTRA_DATE, quote.date)
+        quoteAdapter = QuoteAdapter(clickListener = object : ClickListener {
+          override fun onClick(quote: Quote) {
+            val intent = Intent(this@MainActivity, AddEditActivity::class.java)
+            intent.apply {
+              putExtra(AddEditActivity.EXTRA_ID, quote.id)
+              putExtra(AddEditActivity.EXTRA_AUTHOR, quote.author)
+              putExtra(AddEditActivity.EXTRA_TEXT, quote.text)
+              putExtra(AddEditActivity.EXTRA_DATE, quote.date)
+            }
+            startActivityForResult(intent, EDIT_QUOTE_REQUEST_CODE)
+          }
+        })
+
+        activityMainBinding.quotesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = quoteAdapter
         }
-        startActivityForResult(intent, EDIT_QUOTE_REQUEST_CODE)
-      }
-    })
 
-    activityMainBinding.quotesRecyclerView.apply {
-      layoutManager = LinearLayoutManager(applicationContext)
-      adapter = quoteAdapter
+        activityMainBinding.addQuoteFloatingButton.setOnClickListener {
+            val intent = Intent(this, AddEditActivity::class.java)
+            startActivityForResult(intent, ADD_QUOTE_REQUEST_CODE)
+        }
+
     }
 
-    activityMainBinding.addQuoteFloatingButton.setOnClickListener {
-      val intent = Intent(this, AddEditActivity::class.java)
-      startActivityForResult(intent, ADD_QUOTE_REQUEST_CODE)
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+              ADD_QUOTE_REQUEST_CODE -> {
+                val intentData = data!!
+                val newNote = Quote(
+                        text = intentData.getStringExtra(AddEditActivity.EXTRA_TEXT)!!,
+                        author = intentData.getStringExtra(AddEditActivity.EXTRA_AUTHOR)!!,
+                        date = intentData.getStringExtra(AddEditActivity.EXTRA_DATE)!!
+                )
+                quotesViewModel.insertQuote(newNote)
+                Toast.makeText(this, "Quote saved!", Toast.LENGTH_SHORT).show()
+              }
+              EDIT_QUOTE_REQUEST_CODE -> {
+                val intentData = data!!
+                val updateQuote = Quote(
+                        id = intentData.getIntExtra(AddEditActivity.EXTRA_ID, -1),
+                        text = intentData.getStringExtra(AddEditActivity.EXTRA_TEXT)!!,
+                        author = intentData.getStringExtra(AddEditActivity.EXTRA_AUTHOR)!!,
+                        date = intentData.getStringExtra(AddEditActivity.EXTRA_DATE)!!
+                )
+                quotesViewModel.updateQuote(updateQuote)
+                Toast.makeText(this, "Quote updated!", Toast.LENGTH_SHORT).show()
+              }
+                else -> {
+                    Toast.makeText(this, "Not found!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
-  }
-
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if (resultCode == Activity.RESULT_OK) {
-      when (requestCode) {
-        ADD_QUOTE_REQUEST_CODE -> {
-          val intentData = data!!
-          val newNote = Quote(
-              text = intentData.getStringExtra(AddEditActivity.EXTRA_TEXT)!!,
-              author = intentData.getStringExtra(AddEditActivity.EXTRA_AUTHOR)!!,
-              date = intentData.getStringExtra(AddEditActivity.EXTRA_DATE)!!
-          )
-          quotesViewModel.insertQuote(newNote)
-          Toast.makeText(this, "Quote saved!", Toast.LENGTH_SHORT).show()
-        }
-        EDIT_QUOTE_REQUEST_CODE -> {
-          val intentData = data!!
-          val updateQuote = Quote(
-              id = intentData.getIntExtra(AddEditActivity.EXTRA_ID, -1),
-              text = intentData.getStringExtra(AddEditActivity.EXTRA_TEXT)!!,
-              author = intentData.getStringExtra(AddEditActivity.EXTRA_AUTHOR)!!,
-              date = intentData.getStringExtra(AddEditActivity.EXTRA_DATE)!!
-          )
-          quotesViewModel.updateQuote(updateQuote)
-          Toast.makeText(this, "Quote updated!", Toast.LENGTH_SHORT).show()
-        }
-        else -> {
-          Toast.makeText(this, "Not found!", Toast.LENGTH_SHORT).show()
-        }
-      }
+    companion object {
+        const val ADD_QUOTE_REQUEST_CODE = 1
+        const val EDIT_QUOTE_REQUEST_CODE = 2
     }
-  }
-
-  companion object {
-    const val ADD_QUOTE_REQUEST_CODE = 1
-    const val EDIT_QUOTE_REQUEST_CODE = 2
-  }
 }
--- app/src/main/java/com/raywenderlich/android/rwquotes/ui/QuoteAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/rwquotes/ui/QuoteAdapter.kt
@@ -48,38 +48,38 @@ import com.raywenderlich.android.rwquotes.databinding.RwquoteItemBinding
 
 class QuoteAdapter(private val clickListener: ClickListener) : RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder>() {
 
-  private var quotes: List<Quote> = listOf()
+    private var quotes: List<Quote> = listOf()
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
-    val rwquoteItemBinding = RwquoteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-    return QuoteViewHolder(rwquoteItemBinding)
-  }
-
-  override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-    val quote = quotes[position]
-    holder.bind(quote)
-  }
-
-  override fun getItemCount(): Int = quotes.size
-
-  fun setQuotes(quotes: List<Quote>) {
-    this.quotes = quotes
-    notifyDataSetChanged()
-  }
-
-  inner class QuoteViewHolder(val rwquoteItemBinding: RwquoteItemBinding) : RecyclerView.ViewHolder(rwquoteItemBinding.root) {
-    fun bind(quote: Quote) {
-      rwquoteItemBinding.quoteText.text = quote.text
-      rwquoteItemBinding.quoteAuthor.text = quote.author
-      rwquoteItemBinding.quoteDate.text = quote.date
-
-      rwquoteItemBinding.root.setOnClickListener {
-        clickListener.onClick(quote)
-      }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
+        val rwquoteItemBinding = RwquoteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return QuoteViewHolder(rwquoteItemBinding)
     }
-  }
+
+    override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
+        val quote = quotes[position]
+        holder.bind(quote)
+    }
+
+    override fun getItemCount(): Int = quotes.size
+
+    fun setQuotes(quotes: List<Quote>) {
+        this.quotes = quotes
+        notifyDataSetChanged()
+    }
+
+    inner class QuoteViewHolder(val rwquoteItemBinding: RwquoteItemBinding) : RecyclerView.ViewHolder(rwquoteItemBinding.root) {
+        fun bind(quote: Quote) {
+            rwquoteItemBinding.quoteText.text = quote.text
+            rwquoteItemBinding.quoteAuthor.text = quote.author
+            rwquoteItemBinding.quoteDate.text = quote.date
+
+            rwquoteItemBinding.root.setOnClickListener {
+                clickListener.onClick(quote)
+            }
+        }
+    }
 }
 
 interface ClickListener {
-  fun onClick(quote: Quote)
+    fun onClick(quote: Quote)
 }
